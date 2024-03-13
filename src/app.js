b2546c4ff17918ec60d5e3000c7d466d098dd115
Dreger <dregerdietle@gmail.com>
2024-03-13 11:36:07 -0700

    Added api skeleton

--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ import swaggerUi from 'swagger-ui-express';
 import yaml from 'yaml';
 import fs from 'fs';
 
+import faceDetection from './routes/face-detection-router';
+
 const logger = CreateLogger('App');
 
 logger.info('API starting up..')
@@ -16,6 +18,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
+
+// Add Routes
+app.use('/', faceDetection);
+
+
 const swaggerFile = fs.readFileSync(path.join(__dirname, '../public/openapi.yaml'), 'utf8');
 const swaggerConfig = yaml.parse(swaggerFile);
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerConfig));
---
+++ src/routes/face-detection-router.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import { CreateLogger } from '../util/logger';
+const router = express.Router();
+const logger = CreateLogger('Face Detection Router');
+
+router.post('/create', async (req, res) => {
+    logger.info('Creating Face Detection Request', req.body);
+    res.status(500).send('Something went wrong');
+});
+router.get('/list', async (req, res) => {
+    res.status(500).send('Something went wrong');
+});
+
+router.get('/status', async (req, res) => {
+    res.status(404).send('Not found');
+});
+router.delete('/delete', async (req, res) => {
+    res.status(404).send('Not found');
+});
+
+export default router;
